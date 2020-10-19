ea642bd89ad6bbaa88361447e4d7113146a3fbe2
alldeltanotheta <magicaltoaster@ymail.com>
2020-10-18 20:04:39 -0400

    50 elevation of barrel complete

--- BattleTank 4.25/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank 4.25/Source/BattleTank/Private/TankBarrel.cpp
@@ -4,6 +4,10 @@
 #include "TankBarrel.h"
 
 void UTankBarrel::Elevate(float RelativeSpeed) {
-	
+	RelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, +1);
+	auto ElevationChange = RelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto RawNewElevation = GetRelativeRotation().Pitch + ElevationChange;
+	auto Elevation = FMath::Clamp<float>(RawNewElevation,minElevationDegrees,MaxElevationDegrees);
+	SetRelativeRotation(FRotator(RawNewElevation,0,0));
 
 }
