c12f12a4c8de02a24e76c97a9d377e519fc505aa
alldeltanotheta <magicaltoaster@ymail.com>
2020-10-19 16:37:39 -0400

    64

--- BattleTank 4.25/Source/BattleTank/Public/TankTrack.h
+++ BattleTank 4.25/Source/BattleTank/Public/TankTrack.h
@@ -17,4 +17,7 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 public:
 	UFUNCTION(BlueprintCallable,Category = Input)
 	void SetThrottle(float Throttle);
+	//max force per track in newtons
+	UPROPERTY(EditDefaultsOnly)
+		float TrackMaxDrivingForce = 40000; // 40 ton tank 1g acceleration
 };
